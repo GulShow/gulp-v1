f5173e8d3386b10de0e7b5c1c04815452d695318
Stanislav Huliaiev <101598952+GulShow@users.noreply.github.com>
2022-09-01 17:42:09 +0300

    remake

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,39 @@
 'use strict';
-const {src , dest} = require('gulp');
+const {src , dest , parallel , watch} = require('gulp');
 const copyScss = () => {
     return src('dist/scss/common/*.scss')
         .pipe(dest('build/styles'))
 }
 
+const sassCompile = (done) => {
+    console.log('Compile SASS to CSS');
+
+    done();
+};
+
+const pugCompile = (done) => {
+    console.log('Compile Pug to HTML');
+
+    done();
+};
+
+const imagesOptimize = (done) => {
+    console.log('Optimize Images');
+
+    done();
+};
+
+const watchers = () => {
+    watch('dist/scss/index.scss', (done) => {
+        console.log('File index.scss was changed');
+        done()
+    })
+}
+
+
+exports.default = parallel(sassCompile, pugCompile, imagesOptimize);
+exports.layoutCompile = parallel(sassCompile, pugCompile);
+exports.assetsOptimize = imagesOptimize;
 exports.copy = copyScss
+exports.watchers = watchers
+
